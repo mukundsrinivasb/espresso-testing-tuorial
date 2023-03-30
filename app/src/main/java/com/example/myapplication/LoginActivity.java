c04f6f8f67fbd3ce475ebaffe627bd871ce5a0c8
Mukund Balaji Srinivas <mukundbalaji.srinivas@anu.edu.au>
2023-03-30 22:15:58 +1100

    Let's see if that works

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -1,15 +1,29 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textview.MaterialTextView;
 
 public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loginactivity);
+        MaterialButton loginButton = findViewById(R.id.button);
+        MaterialTextView loginView = findViewById(R.id.login);
+        MaterialTextView passWord = findViewById(R.id.password);
+        loginButton.setOnClickListener(v -> {
+                    if (loginView.getText().toString().equals("this@that.com") && passWord.getText().toString().equals("password")) {
+                        Toast.makeText(this,"Well that checks out",Toast.LENGTH_LONG).show();
+                    }
+                }
+
+        );
 
     }
 }
